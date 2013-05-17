13888d1d0ae8f39bfc95dedfe9b68ba1897f02ce
Chase <chase@conklins.net>
2013-05-17 18:31:57 -0400

    Made upload path dynamic to the root of the rails app.

--- app/uploaders/book_uploader.rb
+++ app/uploaders/book_uploader.rb
@@ -18,7 +18,7 @@ class BookUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/Users/chase/Rails/books/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.root.join("uploads", model.class.to_s.underscore, mounted_as.to_s, model.id.to_s).to_s
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
