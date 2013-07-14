c85bfc3c727b5dceea5d8a6c632d1aca2e137ff8
Chase Conklin <chase@conklins.net>
2013-07-13 22:03:08 -0400

    Fixed map being called on string.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def tag_path(tag)
-    if params[:tags]
+    if params[:tags] && params[:tags].respond_to?(:map)
       books_path(tags: params[:tags].map(&:to_i).append(tag.id).uniq)
     else
       books_path(tags: (Array tag.id))
@@ -9,6 +9,7 @@ module ApplicationHelper
   end
   
   def remove_tag_path(tag)
+    return books_path unless params[:tags].respond_to?(:map)
     tag_ids = params[:tags].map(&:to_i)
     tag_ids.delete(tag.id)
     tag_ids.empty? ? books_path : books_path(tags: tag_ids)
